7353258f63b577f07ab4544375ac680eacaf4f86
Diego Bejarano <diegoalejandrop78@gmail.com>
2026-02-13 08:41:41 -0600

    Laboratorio 4 de programacion, Actividad: Juego Ahorcado, Trabaje solo

---
+++ src/lab4p2_diegobejarano/juegoahorcado/ExceptionEntradaInvalida.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab4p2_diegobejarano.juegoahorcado;
+
+/**
+ *
+ * @author diego
+ */
+public class ExceptionEntradaInvalida extends Exception{
+    public ExceptionEntradaInvalida(String mensaje){
+        super(mensaje);
+    }
+}
